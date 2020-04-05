2737e97e51b7c3ffe4b0318b89ad4200c9bb61c1
Serhii Zahorui <zahoruiserhii@gmail.com>
2020-04-06 00:25:24 +0300

    Fixed authentication issue

--- src/main/java/com/nerdysoft/taskmanager/security/SecurityConfiguration.java
+++ src/main/java/com/nerdysoft/taskmanager/security/SecurityConfiguration.java
@@ -1,35 +1,46 @@
 package com.nerdysoft.taskmanager.security;
 
+import com.nerdysoft.taskmanager.service.MongoUserDetailsService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.inject.Inject;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().;
-//    }
+    @Inject
+    private MongoUserDetailsService mongoUserDetailsService;
 
+    @Inject
+    private PasswordEncoder encoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .userDetailsService(mongoUserDetailsService)
+            .passwordEncoder(encoder);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
                 .antMatchers("/users/registration").permitAll()
-                .anyRequest().permitAll()
+                .antMatchers("/users").hasAuthority("user")
+//                .anyRequest().hasAnyAuthority("user")
                 /*.antMatchers("/users/{username}").authenticated()*/
+                .and()
+            .httpBasic()
                 .and()
             .csrf()
                 .disable();
     }
 
-    @Bean
-    public BCryptPasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
